8adf73a56bb15861cc2a730f45e909dd80c3e2f2
FuncSmile <user.fadli3001@gmail.com>
2026-02-20 23:54:52 +0700

    refactor: remove navigation menu component and clean up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+
 export default function Home() {
   return (
     <>
-      <header>
-        <h1 className="text-4xl font-bold text-center mt-10">
-          SHO SHA LAUNDRY
-        </h1>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </header>
+    <header>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        SHO SHA LAUNDRY
+      </h1>
+    </header>
     </>
   );
 }
